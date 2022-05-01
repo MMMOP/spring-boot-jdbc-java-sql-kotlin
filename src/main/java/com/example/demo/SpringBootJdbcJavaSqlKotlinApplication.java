f21ab6c1e0e97ff7f6cd04d17cdd29b183dd66f7
winweb <theerasun@gmail.com>
2022-05-01 09:03:28 +0700

    Revert "use FluentNamedParameterJdbcTemplate."
    This reverts commit 85608fd20773d4fa52e56b86f6b216b8159acec1.

--- src/main/java/com/example/demo/SpringBootJdbcJavaSqlKotlinApplication.java
+++ src/main/java/com/example/demo/SpringBootJdbcJavaSqlKotlinApplication.java
@@ -1,23 +1,28 @@
 package com.example.demo;
 
-import com.clevergang.jdbc.FluentNamedParameterJdbcTemplate;
 import com.example.demo.model.Beer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
+import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringBootJdbcJavaSqlKotlinApplication {
 
 	@Autowired
-	private FluentNamedParameterJdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJdbcJavaSqlKotlinApplication.class, args);
@@ -28,38 +33,31 @@ public class SpringBootJdbcJavaSqlKotlinApplication {
 
 		Init() {
 			//Create the database table:
-			jdbcTemplate.update("DROP TABLE IF EXISTS beers").execute();
-			jdbcTemplate.update(KotlinSql.Companion.getCreate()).execute();
+			jdbcTemplate.execute("DROP TABLE IF EXISTS beers");
+			jdbcTemplate.execute(KotlinSql.Companion.getCreate());
 
 
 			//Insert many record:
+			Map<String, Object> parameters = new HashMap<>();
 			for (int i = 0; i < 10; i++) {
-				jdbcTemplate.update(KotlinSql.Companion.getInsert())
-					.bind("name","name " + i)
-					.bind("no", i)
-					.execute();
+				parameters.put("name","name " + i);
+				parameters.put("no", i);
+				namedParameterJdbcTemplate.update(KotlinSql.Companion.getInsert(), parameters);
 			}
 
 
 			//Read records:
-			List<Beer> beers = jdbcTemplate.query(KotlinSql.Companion.select())
-					.bind("no", 10)
-					.fetch(Beer.class);
+			List<Beer> beers = namedParameterJdbcTemplate.query(KotlinSql.Companion.select(),
+				new MapSqlParameterSource()
+					.addValue("no", 10),
+				(resultSet, rowNum) -> new Beer(
+					resultSet.getString("name"),
+					resultSet.getInt("no")
+				));
 
 
 			//Print read records:
 			beers.forEach(System.out::println);
 		}
-	}
-
-	@Bean
-	@Primary
-	public NamedParameterJdbcTemplate getJdbcTemplate(DataSource dataSource) {
-		return new NamedParameterJdbcTemplate(dataSource);
-	}
-
-	@Bean
-	public FluentNamedParameterJdbcTemplate getFluentJdbcTemplate(NamedParameterJdbcTemplate existingJdbcTemplate) {
-		return new FluentNamedParameterJdbcTemplate(existingJdbcTemplate.getJdbcOperations());
 	}
 }
--- src/main/java/com/example/demo/model/Beer.java
+++ src/main/java/com/example/demo/model/Beer.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
-@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @ToString
 public class Beer{
